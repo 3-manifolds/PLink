ebf60b507529aca0dd10fbc0f705cce432da6540
Nathan Dunfield <nathan@dunfield.info>
2009-04-30 09:05:29 -0500

    Added py2app support for building a real Mac application

---
+++ plink-app/PLink.py
@@ -0,0 +1,37 @@
+"""
+Here is what I know about py2app so far.  
+
+First, I started with PLink because it's simpler.  After
+easy_installing py2app, I created the plink-app directory contain a
+three line file "PLink.py" which imports plink and invokes the editor.
+The quickest thing to do is then just type
+
+py2applet PLink.py
+
+which builds PLink.app which is truly stand-alone, containing a copy
+of Python and Tk.  If you don't want to do this, just specify the flag
+"--semi-standalone".
+
+Or one can go the setup.py route with 
+
+py2applet --make-setup PLink.py
+
+and then 
+
+python setup.py py2app
+
+builds dist/PLink.app.  
+
+Of course, one can also add an icon, and I did that too, with a final py2applet command of:
+
+py2applet --make-setup --semi-standalone --iconfile=plink.icns PLink.py
+
+One annoyance is that py2app adds a "console window".  I suppressed
+this by adding a forth line to PLink.py
+"""
+
+
+import plink
+LE = plink.LinkEditor()
+LE.window.tk.call('console', 'hide')  # Gets rid of the silly console Window.  
+LE.window.mainloop()
