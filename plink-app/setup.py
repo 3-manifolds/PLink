662f244fb1d0bbacb39f69147ce0c0b8e06f0ff4
Nathan Dunfield <nathan@dunfield.info>
2009-04-30 17:49:50 -0500

    reverted main setup.py, minor tweak of other one

--- plink-app/setup.py
+++ plink-app/setup.py
@@ -5,15 +5,30 @@ Usage:
     python setup.py py2app
 """
 
-from setuptools import setup
+from setuptools import setup, Command
+import os
 
 APP = ['PLink.py']
 DATA_FILES = []
-OPTIONS = {'argv_emulation': True, 'iconfile': 'plink.icns'}
+OPTIONS = {'argv_emulation': True, 'iconfile': 'plink.icns', 'semi_standalone' : True}
 
+class clean(Command):
+    user_options = []
+
+    def initialize_options(self):
+        pass 
+        
+    def finalize_options(self):
+        pass
+    
+    def run(self):
+        os.system("rm -rf build dist")
+    
+    
 setup(
     app=APP,
     data_files=DATA_FILES,
     options={'py2app': OPTIONS},
     setup_requires=['py2app'],
+    cmdclass   = {'clean' : clean},
 )
