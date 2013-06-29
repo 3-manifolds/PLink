9ac15c94fef28177d7b309ebf073b36d976d9942
Marc Culler <culler@users.noreply.github.com>
2013-06-29 08:24:55 -0500

    Fix round-off artifacts in zoom_to_fit.

--- plink/__init__.py
+++ plink/__init__.py
@@ -819,14 +819,11 @@ class LinkEditor:
 
     def _zoom(self, xfactor, yfactor):
         ulx, uly, lrx, lry = self.canvas.bbox('transformable')
-        # this scales the frozen arrows.  We redraw the rest again.
-        #self.canvas.scale('transformable', ulx, uly, xfactor, yfactor)
         for vertex in self.Vertices:
             vertex.x = ulx + xfactor*(vertex.x - ulx)
             vertex.y = uly + yfactor*(vertex.y - uly)
         self.update_crosspoints()
         for arrow in self.Arrows:
-#            self.update_crossings(arrow)
             arrow.draw(self.Crossings, skip_frozen=False)
         for vertex in self.Vertices:
             vertex.draw(skip_frozen=False)
@@ -845,12 +842,19 @@ class LinkEditor:
 
     def zoom_to_fit(self):
         W, H = self.canvas.winfo_width(), self.canvas.winfo_height()
-        x0, y0, x1, y1 = self.canvas.bbox('transformable')
-        w, h = float(x1-x0), float(y1-y0)
+        # To avoid round-off artifacts, compute a floating point bbox
+        x0, y0, x1, y1 = W, H, 0, 0
+        for V in self.Vertices:
+            x0, y0 = min(x0, V.x), min(y0, V.y)
+            x1, y1 = max(x1, V.x), max(y1, V.y)
+        w, h = x1-x0, y1-y0
         factor = min( (W-40)/w, (H-40)/h )
-        self._shift( (W - factor*w)/2 - x0,
-                     (H - factor*h)/2 - y0 )
-        self._zoom(factor, factor)
+        # Make sure we get an integer bbox after zooming
+        xfactor, yfactor = round(factor*w)/w, round(factor*h)/h
+        self._zoom(xfactor, yfactor)
+        # Now center the picture
+        x0, y0, x1, y1 = self.canvas.bbox('transformable')
+        self._shift( (W - x1 + x0)/2 - x0, (H - y1 + y0)/2 - y0 )
 
     def update_info(self):
         self.hide_DT()
