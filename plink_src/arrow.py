9ad7b6882e695840c008173f7a54aa39615272d0
Marc Culler <culler@users.noreply.github.com>
2015-11-01 09:24:22 -0600

    Making the file structure more logical, less monolithic.

---
+++ plink_src/arrow.py
@@ -0,0 +1,199 @@
+from math import sqrt
+from gui import *
+default_gap_size = 9.0
+
+class Arrow:
+    """
+    An arrow in a PL link diagram.
+    """
+    epsilon = 8
+    
+    def __init__(self, start, end, canvas=None, style='normal', color='black'):
+        self.start, self.end = start, end
+        self.canvas = canvas
+        self.color = color
+        self.component = None
+        self.style = 'normal'
+        self.lines = []
+        self.dots = []
+        self.cross_params = []
+        if self.start != self.end:
+            self.start.out_arrow = self
+            self.end.in_arrow = self
+            self.vectorize()
+        
+    def __repr__(self):
+        return '%s-->%s'%(self.start, self.end)
+
+    def __xor__(self, other):
+        """
+        Returns the barycentric coordinate at which self crosses other.
+        """
+        D = other.dx*self.dy - self.dx*other.dy
+        if D == 0:
+            return None
+        xx = other.start.x - self.start.x
+        yy = other.start.y - self.start.y
+        s = (yy*self.dx - xx*self.dy)/D
+        t = (yy*other.dx - xx*other.dy)/D
+        if 0 < s < 1 and 0 < t < 1:
+            return t
+        else:
+            return None
+
+    def vectorize(self):
+        self.dx = float(self.end.x - self.start.x)
+        self.dy = float(self.end.y - self.start.y)
+        self.length = sqrt(self.dx*self.dx + self.dy*self.dy) 
+
+    def reverse(self, crossings=[]):
+        self.end, self.start = self.start, self.end
+        self.vectorize()
+        self.draw(crossings)
+
+    def hide(self):
+        for line in self.lines:
+            self.canvas.delete(line)
+            self.style = 'hidden'
+
+    @property
+    def hidden(self):
+        return self.style == 'hidden'
+
+    def freeze(self):
+        for line in self.lines:
+            self.canvas.itemconfig(line, fill='gray')
+        self.style = 'frozen'
+
+    @property
+    def frozen(self):
+        return self.style == 'frozen'
+
+    def make_faint(self):
+        for line in self.lines:
+            self.canvas.itemconfig(line, fill='gray', width=1)
+        self.style = 'faint'
+
+    def expose(self, crossings=[]):
+        self.style = 'normal'
+        self.draw(crossings)
+
+    def find_segments(self, crossings, include_overcrossings=False,
+                      gapsize=default_gap_size):
+        """
+        Return a list of segments that make up this arrow, each
+        segment being a list of 4 coordinates [x0,y0,x1,y1].  The
+        first segment starts at the start vertex, and the last one
+        ends at the end vertex.  Otherwise, endpoints are near
+        crossings where this arrow goes under, leaving a gap between
+        the endpoint and the crossing point.  If the
+        include_overcrossings flag is True, then the segments are
+        also split at overcrossings, with no gap.
+        """
+        segments = []
+        self.vectorize()
+        cross_params = [(0.0,False), (1.0,False)]
+        for c in crossings:
+            if c.under == self:
+                t = self ^ c.over
+                if t:
+                    cross_params.append((t, not c.is_virtual))
+            if c.over == self and include_overcrossings:
+                t = self ^ c.under
+                if t:
+                    cross_params.append((t, False))
+        cross_params.sort()
+        
+        def r(t):
+            "Affine parameterization of the arrow with domain [0,1]."
+            if t == 1.0:
+                return list(self.end.point())
+            x, y = self.start.point()
+            return [x + t*self.dx, y + t*self.dy]
+
+        def gap(dt):
+            "A suitable gap for r restricted to a subinterval of length dt"
+            return min(gapsize/self.length, 0.2*dt)
+
+        segments = []
+        for i in range(len(cross_params)-1):
+            a, has_gap_a = cross_params[i]
+            b, has_gap_b = cross_params[i+1]
+            gap_a = gap(b-a) if has_gap_a else 0
+            gap_b = gap(b-a) if has_gap_b else 0
+            segments.append( (a + gap_a, b - gap_b) )
+        return [r(a) + r(b) for a, b in segments]
+
+    def draw(self, crossings=[], recurse=True, skip_frozen=True):
+        if self.hidden or (self.frozen and skip_frozen):
+            return
+        if self.style == 'frozen':
+            color = 'gray'
+            thickness = 3
+        elif self.style == 'faint':
+            color = 'gray'
+            thickness = 1
+        else:
+            color = self.color
+            thickness = 3
+        segments = self.find_segments(crossings)
+        for line in self.lines:
+            self.canvas.delete(line)
+        for dot in self.dots:
+            self.canvas.delete(dot)
+        for x0, y0, x1, y1 in segments[:-1]:
+            self.lines.append(self.canvas.create_line(
+                    x0, y0, x1, y1,
+                    width=thickness, fill=color, tags='transformable'))
+        x0, y0, x1, y1 = segments[-1]
+        self.lines.append(self.canvas.create_line(
+                x0, y0, x1, y1,
+                arrow=Tk_.LAST,
+                width=thickness, fill=color, tags='transformable'))
+        if recurse:
+            under_arrows = [c.under for c in crossings if c.over == self]
+            for arrow in under_arrows:
+                arrow.draw(crossings, recurse=False)
+        for c in crossings:
+            if self == c.under and c.is_virtual:
+                self.dots.append(self.canvas.create_oval(
+                        c.x-5, c.y-5, c.x+5, c.y+5,
+                        fill='black', outline='black',
+                        tags=('dot', 'transformable')))
+        self.canvas.tag_raise('dot', Tk_.ALL)
+    
+    def set_start(self, vertex, crossings=[]):
+        self.start = vertex
+        if self.end:
+            self.vectorize()
+            self.draw(crossings)
+
+    def set_end(self, vertex, crossings=[]):
+        self.end = vertex
+        if self.start:
+            self.vectorize()
+            self.draw(crossings)
+
+    def set_color(self, color):
+        self.color = color
+        for line in self.lines:
+            self.canvas.itemconfig(line, fill=color)
+            
+    def erase(self):
+        """
+        Prepare the arrow for the garbage collector.
+        """
+        self.start = None
+        self.end = None
+        self.hide()
+
+    def too_close(self, vertex, tolerance=None):
+        if vertex == self.start or vertex == self.end:
+            return False
+        e = tolerance if tolerance else Arrow.epsilon
+        Dx = vertex.x - self.start.x
+        Dy = vertex.y - self.start.y
+        A = (Dx*self.dx + Dy*self.dy)/self.length
+        B = (Dy*self.dx - Dx*self.dy)/self.length
+        return (-e < A < self.length + e and -e < B < e)
+
---
+++ plink_src/gui.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import sys
+from colorsys import hls_to_rgb
+
+try: 
+    if sys.version_info[0] < 3:
+        import Tkinter as Tk_
+        import tkFileDialog
+        import tkMessageBox
+        import tkSimpleDialog
+    else:
+        import tkinter as Tk_
+        import tkinter.filedialog as tkFileDialog
+        import tkinter.messagebox as tkMessageBox
+        import tkinter.simpledialog as tkSimpleDialog
+    from . import canvasvg
+except ImportError:  # Tk unavailable or misconfigured
+    Tk_, tkFileDialog, tkMessageBox, tkSimpleDialog, canvasvg = None, None, None, None, None
+
+try:
+    from urllib import pathname2url
+except:  # Python 3
+    from urllib.request import pathname2url
+
+try:
+    import pyx
+    have_pyx = True
+except ImportError:
+    have_pyx = False
+
+if sys.platform == 'linux2':
+    closed_hand_cursor = 'fleur'
+    open_hand_cursor = 'hand1'
+elif sys.platform == 'darwin':
+    closed_hand_cursor = 'closedhand'
+    open_hand_cursor = 'openhand'
+else:
+    closed_hand_cursor = 'hand2'
+    open_hand_cursor = 'hand1'
+    
+# Make the Tk file dialog work better with file extensions on OX
+
+def asksaveasfile(mode='w',**options):
+    """
+    Ask for a filename to save as, and returned the opened file.
+    Modified from tkFileDialog to more intelligently handle
+    default file extensions. 
+    """
+    if sys.platform == 'darwin':
+        if 'defaultextension' in options and not 'initialfile' in options:
+            options['initialfile'] = 'untitled' + options['defaultextension']
+
+    return tkFileDialog.asksaveasfile(mode=mode, **options)
+
+if sys.platform == 'linux2':
+    def askopenfile():
+        return tkFileDialog.askopenfile(
+            mode='r',
+            title='Open SnapPea Projection File')
+else:
+    def askopenfile():
+        return tkFileDialog.askopenfile(
+            mode='r',
+            title='Open SnapPea Projection File',
+            defaultextension = ".lnk",
+            filetypes = [
+                ("Link and text files", "*.lnk *.txt", "TEXT"),
+                ("All text files", "", "TEXT"),
+                ("All files", "")],
+            )
+
+# Keyboard shortcuts
+scut = {
+    'Left'   : '←',
+    'Up'     : '↑',
+    'Right'  : '→',
+    'Down'   : '↓'}
+
+# Shift vectors
+canvas_shifts = {
+    'Down'  : (0, 5),
+    'Up'    : (0, -5),
+    'Right' : (5, 0),
+    'Left'  : (-5, 0)
+    }
+vertex_shifts = {
+    'Down'  : (0, 1),
+    'Up'    : (0, -1),
+    'Right' : (1, 0),
+    'Left'  : (-1, 0)
+    }
+
