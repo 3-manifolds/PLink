d743e9ebf2878174d8dd3fe3d174770712a615cd
Marc Culler <culler@users.noreply.github.com>
2015-11-01 19:23:00 -0600

    Added consistent copyright / license notices to each source file.

--- plink_src/crossings.py
+++ plink_src/crossings.py
@@ -1,3 +1,22 @@
+#
+#   Copyright (C) 2007-2009 Marc Culler, Nathan Dunfield and others.
+#
+#   This program is distributed under the terms of the 
+#   GNU General Public License, version 2 or later, as published by
+#   the Free Software Foundation.  See the file gpl-2.0.txt for details.
+#   The URL for this program is
+#     http://www.math.uic.edu/~t3m/plink
+#   A copy of the license file may be found at:
+#     http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#
+#   The development of this program was partially supported by
+#   the National Science Foundation under grants DMS0608567,
+#   DMS0504975 and DMS0204142.
+"""
+This module exports the Crossing class, which represents a crossing
+in a link diagram, and the ECrossing class which represents an edge
+of the diagram passing through a crossing.
+"""
 class Crossing:
     """
     A pair of crossing arrows in a PL link diagram.
--- plink_src/dialog.py
+++ plink_src/dialog.py
@@ -1,3 +1,21 @@
+#
+#   Copyright (C) 2007-2009 Marc Culler, Nathan Dunfield and others.
+#
+#   This program is distributed under the terms of the 
+#   GNU General Public License, version 2 or later, as published by
+#   the Free Software Foundation.  See the file gpl-2.0.txt for details.
+#   The URL for this program is
+#     http://www.math.uic.edu/~t3m/plink
+#   A copy of the license file may be found at:
+#     http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+#
+#   The development of this program was partially supported by
+#   the National Science Foundation under grants DMS0608567,
+#   DMS0504975 and DMS0204142.
+"""
+This module exports the InfoDialog class, used for displaying information
+about the PLink program.
+"""
 # Hack for when Tkinter is unavailable or broken
 from gui import *
 
