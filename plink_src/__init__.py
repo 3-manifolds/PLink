ab06175eb8d83272c03943cf1a15628ddf401c13
Marc Culler <Marc Culler@localhost>
2019-08-05 12:30:05 -0500

    Insted, fix the issue which was causing the ImportError.

--- plink_src/__init__.py
+++ plink_src/__init__.py
@@ -15,14 +15,10 @@
 #   DMS0504975 and DMS0204142.
 
 from .version import version as __version__
-try:
-    from . import gui
-    from .manager import LinkManager
-    from .viewer import LinkViewer
-    from .editor import LinkDisplay, LinkEditor
-except ImportError:
-    # Tk is not available
-    LinkManager = LinkViewer = LinkDisplay = LinkEditor = None
+from . import gui
+from .manager import LinkManager
+from .viewer import LinkViewer
+from .editor import LinkDisplay, LinkEditor
     
 __all__ = ['LinkManager', 'LinkViewer', 'LinkDisplay', 'LinkEditor']
 
